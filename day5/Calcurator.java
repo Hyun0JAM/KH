cb978afd2f0e287e2674dc1da6cc8b6a83f57731
Hyun0JAM <42133544+Hyun0JAM@users.noreply.github.com>
2018-08-10 17:36:38 +0900

    2018.08.10

--- day5/Calcurator.java
+++ day5/Calcurator.java
@@ -12,11 +12,15 @@ public class Calcurator {
 		case "-": result = a-b; break;
 		case "*": result = a*b; break;
 		case "/": 
-				try{result = a/b; 
+			if(b==0) return "두번 째 인자 값b는 0이 될수 없다.";
+			else result = a/b; break;
+			
+/*				try{result = a/b; 
 					break;}
 				catch(ArithmeticException e) {
 					//분모가 0 일때 발생_ 어떤숫자를 0으로 나눌 때
-					return "두번 째 인자 값b는 0이 될수 없다.";}
+					return "두번 째 인자 값b는 0이 될수 없다.";}*/
+			
 		default : break;}
 		
 		if(result%(int)result ==0) return Integer.toString((int)result); //소수부가 .0인경우
